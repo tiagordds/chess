2335e57e317be540c040141f44ee0ab89a013a1d
Tiago Rodrigues <yutri.trds@gmail.com>
2024-11-23 18:38:17 -0300

    With notation

--- chess_main.py
+++ chess_main.py
@@ -1,18 +1,28 @@
 import PySimpleGUI as sg
 
+pieces_initial_positions = {
+    (0, 0): "W_Rook",
+    (0, 1): "W_Knight",
+    (0, 3): "W_Bishop",
+    (0, 4): "W_Queen",
+    (0, 5): "W_King",
+    (0, 6): "W_Bishop",
+    (0, 7): "W_Knight",
+    (0, 8): "W_Rook",
+}
+
 
 def create_chessboard():
     board = []
-    unicode_conveter = [72, 71, 70, 69, 68, 67, 66, 65]
-    for row in unicode_conveter:
+    for row in range(72, 64, -1):
         row_layout = []
-        for col in range(8):
-            square_color = "white" if (row + col) % 2 == 0 else "gray"
+        for col in range(1, 9):
+            square_color = "gray" if (row + col) % 2 == 0 else "white"
             row_layout.append(
                 sg.Button(
                     "",
                     size=(10, 5),
-                    key=((col + 1), chr(row)),
+                    key=((col), chr(row)),
                     button_color=("black", square_color),
                 )
             )
