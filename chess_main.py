23c141ae503b9a898e988afb0ab600c6a7afb057
Tiago Rodrigues <yutri.trds@gmail.com>
2024-11-23 19:26:11 -0300

    creates the board with pieces in the right place

--- chess_main.py
+++ chess_main.py
@@ -1,56 +1,70 @@
 import PySimpleGUI as sg
 
 pieces_initial_positions = {
-    (0, 0): "B_Rook",
-    (1, 0): "B_Knight",
-    (2, 0): "B_Bishop",
-    (3, 0): "B_Queen",
-    (4, 0): "B_King",
-    (5, 0): "B_Bishop",
-    (6, 0): "B_Knight",
-    (7, 0): "B_Rook",
-    (0, 1): "B_Pawn",
-    (1, 1): "B_Pawn",
-    (2, 1): "B_Pawn",
-    (3, 1): "B_Pawn",
-    (4, 1): "B_Pawn",
-    (5, 1): "B_Pawn",
-    (6, 1): "B_Pawn",
-    (7, 1): "B_Pawn",
-    (0, 7): "W_Rook",
-    (1, 7): "W_Knight",
-    (2, 7): "W_Bishop",
-    (3, 7): "W_Queen",
-    (4, 7): "W_King",
-    (5, 7): "W_Bishop",
-    (6, 7): "W_Knight",
-    (7, 7): "W_Rook",
-    (0, 6): "W_Pawn",
-    (1, 6): "W_Pawn",
-    (2, 6): "W_Pawn",
-    (3, 6): "W_Pawn",
-    (4, 6): "W_Pawn",
-    (5, 6): "W_Pawn",
-    (6, 6): "W_Pawn",
-    (7, 6): "W_Pawn",
+    (0, 0): "♜",
+    (1, 0): "♞",
+    (2, 0): "♝",
+    (3, 0): "♛",
+    (4, 0): "♚",
+    (5, 0): "♝",
+    (6, 0): "♞",
+    (7, 0): "♜",
+    (0, 1): "♟",
+    (1, 1): "♟",
+    (2, 1): "♟",
+    (3, 1): "♟",
+    (4, 1): "♟",
+    (5, 1): "♟",
+    (6, 1): "♟",
+    (7, 1): "♟",
+    (0, 7): "♖",
+    (1, 7): "♘",
+    (2, 7): "♗",
+    (3, 7): "♕",
+    (4, 7): "♔",
+    (5, 7): "♗",
+    (6, 7): "♘",
+    (7, 7): "♖",
+    (0, 6): "♙",
+    (1, 6): "♙",
+    (2, 6): "♙",
+    (3, 6): "♙",
+    (4, 6): "♙",
+    (5, 6): "♙",
+    (6, 6): "♙",
+    (7, 6): "♙",
 }
 
 
 def create_chessboard():
     board = []
-    for row in range(8):
-        row_layout = []
+    for line in range(8):
+        line_layout = []
         for col in range(8):
-            square_color = "white" if (row + col) % 2 == 0 else "gray"
-            row_layout.append(
-                sg.Button(
-                    "",
-                    size=(10, 5),
-                    key=((col), row),
-                    button_color=("black", square_color),
+            square_color = "white" if (line + col) % 2 == 0 else "gray"
+            if (col, line) in pieces_initial_positions.keys():
+                line_layout.append(
+                    sg.Button(
+                        pieces_initial_positions[col, line],
+                        size=(10, 5),
+                        key=(col, line),
+                        button_color=("black", square_color),
+                        font=("Arial", 13),
+                        auto_size_button=False,
+                    )
                 )
-            )
-        board.append(row_layout)
+            else:
+                line_layout.append(
+                    sg.Button(
+                        "",
+                        size=(10, 5),
+                        key=(col, line),
+                        font=("Arial", 13),
+                        auto_size_button=False,
+                        button_color=("black", square_color),
+                    )
+                )
+        board.append(line_layout)
     return board
 
 
