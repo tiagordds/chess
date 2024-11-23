cdc14075b4c2029f6d2c02f8187ba8bf9fb18e28
Tiago Rodrigues <yutri.trds@gmail.com>
2024-11-23 18:50:25 -0300

    piece notation done

--- chess_main.py
+++ chess_main.py
@@ -1,28 +1,52 @@
 import PySimpleGUI as sg
 
 pieces_initial_positions = {
-    (0, 0): "W_Rook",
-    (0, 1): "W_Knight",
-    (0, 3): "W_Bishop",
-    (0, 4): "W_Queen",
-    (0, 5): "W_King",
-    (0, 6): "W_Bishop",
-    (0, 7): "W_Knight",
-    (0, 8): "W_Rook",
+    (0, 0): "B_Rook",
+    (1, 0): "B_Knight",
+    (2, 0): "B_Bishop",
+    (3, 0): "B_Queen",
+    (4, 0): "B_King",
+    (5, 0): "B_Bishop",
+    (6, 0): "B_Knight",
+    (7, 0): "B_Rook",
+    (0, 1): "B_Pawn",
+    (1, 1): "B_Pawn",
+    (2, 1): "B_Pawn",
+    (3, 1): "B_Pawn",
+    (4, 1): "B_Pawn",
+    (5, 1): "B_Pawn",
+    (6, 1): "B_Pawn",
+    (7, 1): "B_Pawn",
+    (0, 7): "W_Rook",
+    (1, 7): "W_Knight",
+    (2, 7): "W_Bishop",
+    (3, 7): "W_Queen",
+    (4, 7): "W_King",
+    (5, 7): "W_Bishop",
+    (6, 7): "W_Knight",
+    (7, 7): "W_Rook",
+    (0, 6): "W_Pawn",
+    (1, 6): "W_Pawn",
+    (2, 6): "W_Pawn",
+    (3, 6): "W_Pawn",
+    (4, 6): "W_Pawn",
+    (5, 6): "W_Pawn",
+    (6, 6): "W_Pawn",
+    (7, 6): "W_Pawn",
 }
 
 
 def create_chessboard():
     board = []
-    for row in range(72, 64, -1):
+    for row in range(8):
         row_layout = []
-        for col in range(1, 9):
-            square_color = "gray" if (row + col) % 2 == 0 else "white"
+        for col in range(8):
+            square_color = "white" if (row + col) % 2 == 0 else "gray"
             row_layout.append(
                 sg.Button(
                     "",
                     size=(10, 5),
-                    key=((col), chr(row)),
+                    key=((col), row),
                     button_color=("black", square_color),
                 )
             )
